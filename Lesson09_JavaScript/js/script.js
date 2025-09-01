f6b959525f1145f1cfa6438551a31511be7cdf67
Hạnh Mai <hanhmai120997@gmail.com>
2025-09-01 22:11:07 +0700

    Create script.js

---
+++ Lesson09_JavaScript/js/script.js
@@ -0,0 +1,6 @@
+var homnay = new Date();
+var hh = homnay.getHours();
+var mm = homnay.getMinutes();
+var ss = homnay.getSeconds();
+
+document.write("<h1>" + hh + " : " + mm + " : " + ss + "</h1>");
